6055b9838f5d0899b63ccc5b270facf6bc6fd319
Aniele004 <anielecamily2984@gmail.com>
2025-03-26 19:21:13 -0300

    Nível Mestre e suas implementações

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 
 
+void moverTorre(int i){
+    if(i == 0){
+        return;
+    } printf("DIREITA\n");
+    moverTorre(i - 1);
+}
+
+void moverBispo(int vertical, int horizontal) {
+    if (vertical == 0) {
+        return; // Condição de parada: sem mais casas para mover
+    }
+
+    printf("CIMA\n");
+    for (int h = 1; h <= horizontal; h++) { // Movendo horizontalmente
+        printf("DIREITA\n");
+    }
+
+    // Chamada recursiva para o próximo movimento vertical
+    moverBispo(vertical - 1, horizontal);
+}
+
+void moverRainha(int r){
+    if(r == 0){
+        return;
+    } printf("CIMA\n");
+    moverRainha(r - 1); // movimento para cima
+}
+
+void moverCavalo() {
+    for (int i = 1; i <= 2; i++) {  // Loop para mover o cavalo 2 casas para cima
+        printf("CIMA\n");
+
+        if (i == 2) {
+            printf("DIREITA\n");
+        }
+
+        if (i == 2) {
+            break;  // Interrompe o loop externo, pois o cavalo já fez seu movimento "L"
+        }
+     } 
+}
+
 
 int main() {
     
@@ -14,47 +56,26 @@ int main() {
 
 
     // torre
-    printf("Movimentação da TORRE (5 casas para a direita)\n");
-    for(int i = 0; i < 5; i++){
-        printf("DIREITA\n");
-    }
+    printf("Movimentação da TORRE (3 casas para a direita)\n");
+    moverTorre(3);
 
     printf("\n");
 
     // bispo
-    int b = 0;
-    printf("Movimentação do BISPO (3 casas para cima/direita)\n");
-    while(b < 3){
-        printf("CIMA/DIREITA\n");
-        b++;
-    }
+    printf("Movimentação do BISPO (6 casas para cima/direita)\n");
+    moverBispo(6, 1);
 
     printf("\n");
 
     // rainha
-    int r = 0;
-    printf("Movimentação da RAINHA (6 casas para cima)\n");
-    do {
-        printf("CIMA\n");
-        r++;
-    }while(r < 6);
+    printf("Movimentação da RAINHA (9 casas para cima)\n");
+    moverRainha(9);
 
     printf("\n");
 
     //cavalo 
     printf("Movimentação do CAVALO (2 casas para baixo e 1 para a esquerda)\n");
-    for( int c = 0; c < 2; c++){
-        printf("BAIXO\n");
-
-        if(c == 1 ){
-                int c = 0; 
-                while(c < 1){
-                printf("ESQUERDA\n");
-                c++;
-            }
-       
-        }
-    } 
+    moverCavalo();
 
     return 0;
 }
